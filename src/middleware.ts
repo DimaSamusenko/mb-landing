7a79bcb7f104d6e034c1a6400f24cc48dc91fddc
samusenko.d <samusenko.d@ifuture.by>
2024-07-18 18:37:10 +0200

    AMRKT-1666 Fix cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-	const visited = request.cookies.get("visited")?.value;
+/*	const visited = request.cookies.get("visited")?.value;
 
 	const redirect = !request.headers.get("referer")?.includes("redirect=false");
 
@@ -14,6 +14,6 @@ export function middleware(request: NextRequest) {
 					new URL("https://www.marathonbet.com/", request.url)
 			  )
 			: NextResponse.next();
-	response.cookies.set("visited", "true");
-	return response;
+	response.cookies.set("visited", "true");*/
+	return NextResponse.next();
 }
