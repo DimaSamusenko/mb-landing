3d2c08d691049c1344b9448770104973f57695eb
samusenko.d <samusenko.d@ifuture.by>
2024-07-20 21:46:21 +0200

    AMRKT-1666 Hide cookie policy component

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,8 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
 	const path = request.nextUrl.pathname;
 
-	console.log(path);
 	const visited = request.cookies.get("visited")?.value;
 	const redirect = request.nextUrl.searchParams.get("redirect") !== "false";
-
-	console.log("is visited", visited);
 
 	const response =
 		visited && redirect
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import '../components/cookie-policy/consent-cookie.css';
+// import '../components/cookie-policy/consent-cookie.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
