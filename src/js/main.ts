d567b0d374a39943cc64ff6bc2842d8ad9a65673
samusenko.d <samusenko.d@ifuture.by>
2024-07-25 15:12:24 +0200

    AMRKT-1666 Add cta script

---
+++ src/js/main.ts
@@ -0,0 +1,32 @@
+export const main = (function main() {
+    if(!document) {
+        return;
+    }
+    function clickHandler(event) {
+        let target = event.target;
+        if (target?.tagName !== 'A') {
+            target = target?.closest("A");
+        }
+
+        if (!target || !target?.classList.contains('cta')) {
+            return;
+        }
+        /*if (!target.classList.contains('cta')) {
+            return window.open(target.href);
+        }*/
+        event.preventDefault();
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+
+        const separator = target.href?.indexOf('?') !== -1 ? "&" : "?";
+        window.open(target.href + separator + urlParams.toString());
+    }
+    document.addEventListener('touchstart', event => {
+        clickHandler(event);
+    });
+    document.addEventListener('click', event => {
+        clickHandler(event);
+    });
+
+}).toString();
+
