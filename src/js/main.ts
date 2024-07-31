7b53d5be99848f826323444ed5fe10d1998ec159
samusenko.d <samusenko.d@ifuture.by>
2024-07-31 15:34:05 +0200

    AMRKT-1666 Handle middle and context clicks on reg button

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,28 +2,47 @@ export const main = (function main() {
     if(!document) {
         return;
     }
-    function clickHandler(event) {
-        let target = event.target;
-        if (target?.tagName !== 'A') {
-            target = target?.closest("A");
-        }
 
-        if (!target || !target?.classList.contains('cta')) {
-            return;
-        }
-        event.preventDefault();
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
+    document.addEventListener('click', clickHandler, false);
+    document.addEventListener('contextmenu', rightClick, false);
+    document.addEventListener('mousedown', middleClick, false);
 
-        const separator = target.href?.indexOf('?') !== -1 ? "&" : "?";
-        window.open(target.href + separator + urlParams.toString());
+    function clickHandler(eventObj) {
+        let target = eventObj.target;
+        let targetAnchor = null;
+        do {
+            if (target && 'A' === target.tagName?.toUpperCase()) {
+                targetAnchor = target;
+                break;
+            }
+            target = target.parentNode;
+        } while (target);
+        if (targetAnchor && targetAnchor?.classList.contains('cta')) {
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const separator = target.href?.indexOf('?') !== -1 ? "&" : "?";
+            const newHref = target.href + separator + urlParams.toString();
+
+            const originalHref = target.href;
+            target.href = newHref;
+            target.target = '_blank';
+            setTimeout(function () {
+                target.href = originalHref;
+            }, 150);
+        }
     }
-    document.addEventListener('touchstart', event => {
-        clickHandler(event);
-    });
-    document.addEventListener('click', event => {
-        clickHandler(event);
-    });
+
+    function middleClick(e) {
+        if (e.which === 2) {
+            clickHandler(e);
+        }
+    }
+
+    function rightClick(e) {
+        if (e.which === 3) {
+            clickHandler(e);
+        }
+    }
 
 }).toString();
 
